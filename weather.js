1c45b4872b38384c9541f9019587c61d3ad99a2e
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-08 11:52:31 -0600

    ajax call

--- weather.js
+++ weather.js
@@ -1,1 +1,16 @@
+// introduce global variables
+let zipCode = $('#zipInput').value
+
+
 // create an ajax call to open weather map
+function getWeather() {
+  return new Promise( function( resolve, reject ) {
+    $.ajax({
+      url: `api.openweathermap.org/data/2.5/weather?zip=${zipCode},us`
+    })
+    .done(function(data, textStatus, XHR){
+      resolve(data)
+      console.log(data)
+    })
+  })
+}
